3b5b0ac2a378fab57e74ed2b0e18f2e5382018b6
LanaStudio <196567654+vadim2807@users.noreply.github.com>
2025-09-22 23:28:46 +0300

    вторая сдача

--- src/components/NoteForm/NoteForm.tsx
+++ src/components/NoteForm/NoteForm.tsx
@@ -1,7 +1,7 @@
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { createNote, type CreateNoteRequest } from '../../services/noteService';
+import { createNote } from '../../services/noteService';
 import { type NoteTag } from '../../types/note';
 import css from './NoteForm.module.css';
 
